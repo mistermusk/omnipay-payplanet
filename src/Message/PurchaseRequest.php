3c7345033b62a9957a19e6f4d39a81e0587923dd
Aleksandr Tikunov <77337707+RubbishCode@users.noreply.github.com>
2023-10-16 23:20:19 +0300

    Update PurchaseRequest.php

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -59,6 +59,88 @@ class PurchaseRequest extends AbstractRequest
 
     }
 
+    public function getSuccessUrl()
+    {
+        return $this->getParameter('success_url');
+    }
+    public function setSuccessUrl($value)
+    {
+        return $this->setParameter('success_url', $value);
+    }
+
+    public function getFailUrl()
+    {
+        return $this->getParameter('fail_url');
+    }
+    public function setFailUrl($value)
+    {
+        return $this->setParameter('fail_url', $value);
+    }
+
+    public function getNotifyUrl()
+    {
+        return $this->getParameter('notify_url');
+    }
+    public function setNotifyUrl($value)
+    {
+        return $this->setParameter('notify_url', $value);
+    }
+
+
+    public function getRouterData()
+    {
+        return $this->getParameter('router_data');
+    }
+    public function setRouterData($value)
+    {
+        return $this->setParameter('router_data', $value);
+    }
+
+
+    public function getCustomData()
+    {
+        return $this->getParameter('custom_data');
+    }
+    public function setCustomData($value)
+    {
+        return $this->setParameter('custom_data', $value);
+    }
+
+    public function getAdditionalData()
+    {
+        return $this->getParameter('additional_data');
+    }
+    public function setAdditionalData($value)
+    {
+        return $this->setParameter('additional_data', $value);
+    }
+
+    public function getAutoRedirect()
+    {
+        return $this->getParameter('auto_redirect');
+    }
+    public function setAutoRedirect($value)
+    {
+        return $this->setParameter('auto_redirect', $value);
+    }
+
+    public function getBuyerId()
+    {
+        return $this->getParameter('buyer_id');
+    }
+    public function setBuyerId($value)
+    {
+        return $this->setParameter('buyer_id', $value);
+    }
+
+    public function getBuyer()
+    {
+        return $this->getParameter('buyer');
+    }
+    public function setBuyer($value)
+    {
+        return $this->setParameter('buyer', $value);
+    }
     public function getData()
     {
         $this->validate('amount', 'currency');
@@ -69,17 +151,16 @@ class PurchaseRequest extends AbstractRequest
             'currency' => $this->getCurrency(),
             'description' => $this->getDescription(),
             'client_id' => $this->getClientId(),
-
-//            'success_url' => $this->getPan(),
-//            'fail_url' => $this->getPan(),
-//            'notify_url' => $this->getPan(),
-//            'router_data' => $this->getPan(),
-//            'custom_data' => $this->getPan(),
-//            'payment_method' => $this->getPan(),
-//            'additional_data' => $this->getPan(),
-//            'auto_redirect' => $this->getPan(),
-//            'buyer_id' => $this->getPan(),
-//            'buyer' => $this->getPan(),
+            'success_url' => $this->getSuccessUrl(),
+            'fail_url' => $this->getFailUrl(),
+            'notify_url' => $this->getNotifyUrl(),
+            'router_data' => $this->getRouterData(),
+            'custom_data' => $this->getCustomData(),
+            'payment_method' => $this->getPaymentMethod(),
+            'additional_data' => $this->getAdditionalData(),
+            'auto_redirect' => $this->getAutoRedirect(),
+            'buyer_id' => $this->getBuyerId(),
+            'buyer' => $this->getBuyer(),
 
         ];
         return array_filter($data, function ($value) {
