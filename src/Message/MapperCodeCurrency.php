a16bf51e0f3a5bc4e448995d7e3b662e87158158
Aleksandr Tikunov <77337707+RubbishCode@users.noreply.github.com>
2023-10-20 20:54:39 +0300

    Create MapperCodeCurrency.php

---
+++ src/Message/MapperCodeCurrency.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Packages\Payments\Helpers;
+
+
+class MapperCodeCurrency
+{
+    protected static $currencyMapper = array(
+        978 => "EUR",
+        "EUR" => 978,
+    );
+
+    public static function convertCurrencyCodeToName(int $code)
+    {
+        return self::$currencyMapper[$code] ?? null;
+    }
+
+    public static function convertCurrencyNameToCode(string $name)
+    {
+        return self::$currencyMapper[$name] ?? null;
+    }
+}
