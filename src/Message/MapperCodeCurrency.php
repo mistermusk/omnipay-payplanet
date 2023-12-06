d8f319c903961732b8af2aef521b0adf46b8b7b6
RubbishCode <rubbishcode@gmail.com>
2023-12-06 21:31:24 +0300

    fix

--- src/Message/MapperCodeCurrency.php
+++ src/Message/MapperCodeCurrency.php
@@ -2,12 +2,13 @@
 
 namespace Omnipay\PayPlanet\Message;
 
+use Alcohol\ISO4217;
 
 class MapperCodeCurrency
 {
     public static function convertCurrencyNameToCode($currencyName)
     {
-        $iso4217 = new Alcohol\ISO4217();
+        $iso4217 = new ISO4217();
 
         $currency = $iso4217->getByAlphabeticCode($currencyName);
 
